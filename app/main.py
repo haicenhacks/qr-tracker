c9481c01b84cfc2b284ae94a95d73be36ddfd949
Haicen <haicenhack@protonmail.com>
2022-01-06 20:37:29 -0500

    added delete function and several other fixes

--- app/main.py
+++ app/main.py
@@ -2,9 +2,6 @@ import json
 import secrets
 import uuid
 import os
-#from app import app
-#from views import *
-#from models import models
 from flask import Flask
 from secrets import users
 
@@ -15,6 +12,10 @@ from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 import flask_login
 
+
+
+
+
 #from . import database
 #from . import models
 app = Flask(__name__)
@@ -23,7 +24,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///qr_project.db'
-app.config['SECRET_KEY'] = "random string"
+app.config['SECRET_KEY'] = os.environ['FLASK_SECRET_KEY']
 
 login_manager = flask_login.LoginManager()
 
@@ -37,7 +38,7 @@ FlaskUUID(app)
 
 
 # You can generate a Token from the "Tokens Tab" in the UI
-url_base = 'http://192.168.1.5:5002/'
+url_base = 'http://192.168.1.5:5002'
 
 
 class User(flask_login.UserMixin):
@@ -64,6 +65,7 @@ def request_loader(request):
     user.id = email
     return user
 
+
 class Qr(db.Model):
     __tablename__ = 'Qr'
     id = db.Column(db.Integer, primary_key=True)
@@ -72,6 +74,7 @@ class Qr(db.Model):
     redirect_uri = db.Column(db.String(255))
     name = db.Column(db.String(100))
     visits = db.relationship('Visitor', backref='qr', cascade = 'all, delete-orphan', lazy = 'dynamic')
+    created_by = db.Column(db.String(100))
 
 
 class Visitor(db.Model):
@@ -84,6 +87,7 @@ class Visitor(db.Model):
     qr_scanned = db.Column(db.Integer, db.ForeignKey(Qr.uuid) )
 
 
+
 @app.route('/hello')
 def hello():
     return "hello world"
@@ -94,15 +98,37 @@ def hello():
 def index():
     #visitors = Visitor.query.all()
     #return render_template('index.html')
-    print(flask_login.current_user.id)
+    #print(flask_login.current_user.id)
     user_ip=request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
     user_agent=str(request.user_agent)
     time = datetime.now()
     return render_template('index.html', user_ip=user_ip, user_agent=user_agent)
+
+
+@app.route('/delete/<uuid:id>')
+def delete_uuid(id):
+
+    if flask_login.current_user.id == 'admin':
+        this_qr = Qr.query.filter_by(uuid = str(id) ).one()
+        db.session.delete(this_qr)
+        db.session.commit()
+
+    else:
+        this_qr = Qr.query.filter_by(uuid=str(id), created_by=flask_login.current_user.id).one()
+        if this_qr:
+            db.session.delete(this_qr)
+            db.session.commit()
+    return redirect('/view')
+
+
 @app.route('/view/<uuid:id>')
 def view_uuid(id):
+
     visitors = Visitor.query.filter_by(uuid = str(id))
-    this_qr = Qr.query.filter_by(uuid = str(id)).one()
+    for q in Qr.query.all():
+        print(q.uuid == str(id), q.uuid, str(id))
+    print(str(id))
+    this_qr = Qr.query.filter_by(uuid=str(id)).one()
 
 
     return render_template('index.html', visitors=visitors, this_qr=this_qr)
@@ -110,8 +136,14 @@ def view_uuid(id):
 
 @app.route('/view')
 def view_campaigns():
-    qrs = Qr.query.all()
+
+    if flask_login.current_user.id == 'admin':
+        print("Admin = true")
+        qrs = Qr.query.all()
+    else:
+        qrs = Qr.query.filter_by(created_by=flask_login.current_user.id)
     return render_template('qr_list.html', qrs=qrs)
+
 
 @app.route('/<uuid:id>')
 def visituuid(id):
@@ -122,8 +154,10 @@ def visituuid(id):
     visitor = Visitor(ip_address=user_ip, user_agent=user_agent, time=time, qr_scanned=str(id), uuid=str(id))
     db.session.add(visitor)
     db.session.commit()
-    this_qr = Qr.query.filter_by(uuid = str(id)).one()
+    this_qr = Qr.query.filter_by(uuid=str(id)).one()
     visitors = Visitor.query.filter_by(uuid = str(id))
+
+
     if str(id) not in this_qr.redirect_uri:
         return redirect(this_qr.redirect_uri, 302)
 
@@ -135,7 +169,7 @@ def visituuid(id):
 def createqr():
 
     random_uuid = uuid.uuid4()
-    qr_uri = f'{url_base}{random_uuid}'
+    qr_uri = f'{url_base}/{random_uuid}'
 
     if request.method == 'POST':
         #if qr_data:
@@ -144,18 +178,25 @@ def createqr():
         name=request.form.get('name')
         if name == '':
             name=None
+
         redirect_uri=request.form.get('redirect_uri')
-        this_qr = Qr(uuid=str(random_uuid), qr_data=qr_uri, name=name, redirect_uri=redirect_uri)
+        if redirect_uri == "":
+            redirect_uri = f'{url_base}{url_for("view_uuid", id=random_uuid)}'
+        this_qr = Qr(uuid=str(random_uuid), qr_data=qr_uri, name=name, redirect_uri=redirect_uri, created_by=flask_login.current_user.id)
+
         db.session.add(this_qr)
         db.session.commit()
         return redirect(url_for('view_uuid',id = str(random_uuid)))
     else:
-        return render_template('create_qr.html', qr_uri=qr_uri, redirect_uri=f'{url_base}view/{random_uuid}')
+        return render_template('create_qr.html', qr_uri=qr_uri, redirect_uri=f'{url_base}/view/{random_uuid}')
+
 
 @app.route('/all')
+@flask_login.login_required
 def all_visits():
     visitors = Visitor.query.all()
     return render_template('all_visits.html', visitors=visitors)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -174,7 +215,7 @@ def login():
         user = User()
         user.id = username
         flask_login.login_user(user)
-        return redirect(url_for('protected'))
+        return redirect(url_for('index'))
 
     return 'Bad login'
 
@@ -184,12 +225,12 @@ def login():
 def protected():
     return 'Logged in as: ' + flask_login.current_user.id
 
+
 @app.route("/logout")
 @flask_login.login_required
 def logout():
     logout_user()
     return redirect(url_for('login'))
-
 
 
 db.create_all()
